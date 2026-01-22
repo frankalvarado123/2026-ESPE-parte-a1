f432862ee9320015a652c0a271d4193277632f99
Frank Marco Alvarado Grefa <frankmarcogrefa@gmail.com>
2026-01-22 07:54:05 -0500

    actualizado 2.0

--- src/test/java/es/upm/grise/profunduzacion/cruiseController/CruiseControlTest.java
+++ src/test/java/es/upm/grise/profunduzacion/cruiseController/CruiseControlTest.java
@@ -8,10 +8,58 @@ class CruiseControlTest {
 	
 	@Test
     public void smokeTest() {}
-	
 
+  @BeforeEach
+    void setUp() {
+        cruiseControl = new CruiseControl();
+    }
+
+    @Test
+    void testSetSpeedSetCorrectWithoutSpeedLimit() throws Exception {
+        cruiseControl.setSpeedSet(80);
+        assertEquals(80, cruiseControl.getSpeedSet());
+    }
+
+    @Test
+    void testSetSpeedSetZeroThrowsException() {
+        assertThrows(IncorrectSpeedSetException.class, () -> {
+            cruiseControl.setSpeedSet(0);
+        });
+    }
+
+   
+    @Test
+    void testSetSpeedSetNegativeThrowsException() {
+        assertThrows(IncorrectSpeedSetException.class, () -> {
+            cruiseControl.setSpeedSet(-10);
+        });
+    }
+
+   
+    @Test
+    void testSetSpeedSetBelowSpeedLimit() throws Exception {
+        cruiseControl.setSpeedLimit(120);
+        cruiseControl.setSpeedSet(100);
+        assertEquals(100, cruiseControl.getSpeedSet());
+    }
+
+    @Test
+    void testSetSpeedSetEqualToSpeedLimit() throws Exception {
+        cruiseControl.setSpeedLimit(120);
+        cruiseControl.setSpeedSet(120);
+        assertEquals(120, cruiseControl.getSpeedSet());
+    }
+
+   
+    @Test
+    void testSetSpeedSetAboveSpeedLimitThrowsException() {
+        cruiseControl.setSpeedLimit(120);
+
+        assertThrows(SpeedSetAboveSpeedLimitException.class, () -> {
+            cruiseControl.setSpeedSet(130);
+        });
+    }
     
-
 }
 
 
